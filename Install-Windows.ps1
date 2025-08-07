c44451d33a9d4a44a995e1e554175e5cb0ca4057
Rob Stamper (PNNL) <rob@pnnl.gov>
2025-08-07 14:48:07 -0700

    Update Install-Windows.ps1

--- Install-Windows.ps1
+++ Install-Windows.ps1
@@ -26,12 +26,12 @@ Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSLanguage en-us -OSEdition Enterp
 
 Write-Host -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
 Write-Host -ForegroundColor Green "We could do something here? Maybe..."
-Start-Sleep -Seconds 5
+Start-Sleep -Seconds 10
 
 $WindowsPath = C:\
-$MSUPath = X:\OSDCloud\Automate\kb5064489.msu
+$MSUPath = D:\OSDCloud\Automate\kb5064489.msu
 dism /Image:$WindowsPath /Add-Package /PackagePath:$MSUPath
 
-Start-Sleep -Seconds 5
+Start-Sleep -Seconds 60
 
 Restart-Computer
